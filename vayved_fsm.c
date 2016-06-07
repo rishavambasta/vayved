4f69ffacc7cf02e0b1123cbe86e3e05723e56b22
Rishav <kooolrishav@gmail.com>
2016-06-08 00:49:06 +0530

    split version on Main daemon

--- vayved_fsm.c
+++ vayved_fsm.c
@@ -4,247 +4,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <netinet/tcp.h>
 #include <string.h>
+#include "vayved.h"
+#include "color.h"
 
-
-#define INTER_POLL_DELAY 1 //seconds
-#define MAX_LINE 500
-#define VERBOSE 1
-/******** paths to led device files ***********************************/
-const char red_led_file[]= "/sys/class/leds/xiaomi:red:status/brightness";
-const char blue_led_file[]= "/sys/class/leds/xiaomi:blue:status/brightness";
-const char yellow_led_file[]= "/sys/class/leds/xiaomi:yellow:status/brightness";
-/**********************************************************************/
-
-//TODO: implement a state called TUNNELLED_BUT_OFFLINE
-//currently its being assumed that tunnelled traffic always findes route to Internet
-typedef enum
-{
-  OFFLINE,
-  TRYING_TO_GO_ONLINE,
-  ONLINE_UNTUNNELLED,
-  TRYING_TO_CREATE_TUNNEL,
-  ONLINE_TUNNELLED
-}FSM_STATES;
 
 FSM_STATES current_state = OFFLINE;
 bool canBlink;
-FILE *red, *blue, *yellow;
-int mutex=0;
 
-
-int dummy_fail_count = 0;
-
-
-const bool on=true;
-const bool off=false;
-
-bool internetConnected (void);
-bool tunnelExists (void);
-int createTunnel(void);
-void resetLeds(void);
-
-
-
-typedef enum
-{
-  RED,
-  ORANGE,
-  VIOLET,
-  BLUE,
-  BLACK
-}Color;
+int pid_of_openVPN_child_process;
 
 Color currentColor = BLACK;
-
-inline void red_led (bool ledSwitch)
-{
-  if (ledSwitch)
-    {
-      fwrite("1",1,1,red); //turn off red
-      fflush(red);
-    }
-  else
-    {
-      fwrite("0",1,1,red); ; //turn off red
-      fflush(red);
-    }
-}
-
-inline void blue_led (bool ledSwitch)
-{
-  if (ledSwitch)
-    {
-      fwrite("1",1,1,blue);
-      fflush(blue);//turn on blue
-    }
-  else
-    {
-      fwrite("0",1,1,blue);
-      fflush(blue);//turn off blue
-    }
-}
-
-inline void yellow_led (bool ledSwitch)
-{
-  if (ledSwitch)
-    {
-      fwrite("1",1,1,yellow);
-      fflush(yellow);//turn on yellow
-    }
-  else
-    {
-      fwrite("0",1,1,yellow);
-      fflush(yellow);//turn off yellow
-    }
-}
-
-void changeColor(Color colorToBeUpdated)
-{
-  if (mutex > 0)
-    printf ("Atomicity violated");
-  ++mutex;
-  switch (colorToBeUpdated)
-    {
-    case RED:
-      switch (currentColor)
-        {
-        case RED:
-          break;
-        case ORANGE:
-          yellow_led(off);
-          red_led(on);
-          break;
-        case VIOLET:
-          blue_led(off);
-          break;
-        case BLUE:
-          blue_led(off);
-          red_led(on);
-          break;
-        case BLACK:
-          red_led(on);
-          break;
-        }
-      break;
-
-    case ORANGE:
-      switch (currentColor)
-        {
-        case RED:
-          red_led(off);
-          yellow_led(on);
-          break;
-        case ORANGE:
-          break;
-        case VIOLET:
-          blue_led(off);
-          red_led(off);
-          yellow_led(on);
-          break;
-        case BLUE:
-          blue_led(off);
-          yellow_led(on);
-          break;
-        case BLACK:
-          yellow_led(on);
-          break;
-        }
-      break;
-
-    case VIOLET:
-      switch (currentColor)
-        {
-        case RED:
-          blue_led(on);
-          break;
-        case ORANGE:
-          yellow_led(off);
-          blue_led(on);
-          red_led(on);
-          break;
-        case VIOLET:
-          break;
-        case BLUE:
-          red_led(on);
-          break;
-        case BLACK:
-          blue_led(on);
-          red_led(on);
-          break;
-        }
-      break;
-
-    case BLUE:
-      switch (currentColor)
-        {
-        case RED:
-          red_led(off);
-          blue_led(on);
-          break;
-        case ORANGE:
-          yellow_led(off);
-          blue_led(on);
-          break;
-        case VIOLET:
-          red_led(off);
-          break;
-        case BLUE:
-          break;
-        case BLACK:
-          blue_led(on);
-          break;
-        }
-      break;
-
-    case BLACK:
-      switch (currentColor)
-        {
-        case RED:
-          red_led(off);
-          break;
-        case ORANGE:
-          yellow_led(off);
-        case VIOLET:
-          blue_led(off);
-          red_led(off);
-          break;
-        case BLUE:
-          blue_led(off);
-          break;
-        case BLACK:
-          break;
-        }
-      break;
-    }
-  currentColor = colorToBeUpdated;
-  --mutex;
-}
-
-char* getColorName (int code)
-{
-  switch (code) {
-    case 0:
-      return "RED";
-      break;
-    case 1:
-      return "ORANGE";
-      break;
-    case 2:
-      return "VIOLET";
-      break;
-    case 3:
-      return "BLUE";
-      break;
-    case 4:
-      return "OFF";
-      break;
-    }
-}
 
 void* vayved_thread (void* threadId)
 {
@@ -280,8 +50,6 @@ void* vayved_thread (void* threadId)
 
 
         case ONLINE_UNTUNNELLED:
-          printf ("\nOnline");
-          fflush(stdout);
           canBlink = false;
           changeColor(BLUE);
           if (internetConnected())
@@ -303,9 +71,16 @@ void* vayved_thread (void* threadId)
             else
               {
                 if (createTunnel() == 1) //Fire OpenVPN Client
-                  next_state = ONLINE_TUNNELLED;
-                else
-                  next_state = ONLINE_UNTUNNELLED;
+                  {
+                    printf ("\nTunnel created\n");
+                    fflush(stdout);
+                    next_state = ONLINE_TUNNELLED;
+                  }
+                else{
+                    printf ("\nTunnel NOT created");
+                    fflush(stdout);
+                    next_state = ONLINE_UNTUNNELLED;
+                  }
               }
           else
             next_state = OFFLINE;
@@ -317,13 +92,21 @@ void* vayved_thread (void* threadId)
           changeColor(VIOLET);
           canBlink = false;
 
-          if (internetConnected())
-            if (tunnelExists())
-              next_state = ONLINE_TUNNELLED;
-            else
-              next_state = ONLINE_UNTUNNELLED;
+          if (tunnelExists())
+            {
+              if (internetConnected())
+                next_state = ONLINE_TUNNELLED;
+              else
+                {
+                  terminate_tunnel();
+                  next_state = OFFLINE;
+                }
+            }
           else
-            next_state = OFFLINE;
+            {
+              terminate_tunnel(); //Just a sanity step, we dont want two openVPN sessions in any case
+              next_state = OFFLINE;
+            }
           break;
         }
       sleep(INTER_POLL_DELAY);
@@ -333,7 +116,7 @@ void* vayved_thread (void* threadId)
 }
 
 
-void* ledBlinkerThread(void *threadID)  //vestigial right now
+void* ledBlinkerThread(void *threadID)  //trivial right now
 {
   Color backupColor;
   while(true)
@@ -343,9 +126,7 @@ void* ledBlinkerThread(void *threadID)  //vestigial right now
       if (canBlink)
         {
           changeColor(BLACK);
-          //dirty flag on
           usleep(50000);
-          //dirty flag off
         }
       if (currentColor == BLACK)
         changeColor(backupColor);
@@ -362,119 +143,80 @@ void exitSignalHandler (int signum)
 {
   printf ("\nExiting with little housekeeping before we go..\n");
   resetLeds();
-  fclose(red);
-  fclose(blue);
-  fclose(yellow);
+  closeLedDeviceFiles();
   exit(0);
 }
 
 
-inline void report_error (char message[])
+
+
+
+void terminate_tunnel()
 {
-  printf ("\n%s",message);
-}
+  int retry = 0;
+  printf ("\nDeleting tunnel\n");
+  fflush(stdout);
+  kill(pid_of_openVPN_child_process, SIGKILL); //TODO:check for boundary cases
 
-
-bool internetConnected ()
-{
-  int sockfd,val;
-  char buffer[MAX_LINE];
-  struct sockaddr_in ipAddress;
-  struct hostent *entity=NULL;
-
-  sockfd = -1;
-
-  if((entity = gethostbyname("www.google.com")) != NULL)
+  while (retry < 500)
     {
-      if((sockfd = socket(entity->h_addrtype,SOCK_STREAM,IPPROTO_TCP)) != -1)
+      if (tunnelExists())
         {
-          val = 1;
-          if(setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR, (char *) &val, sizeof(val)) == 0 && setsockopt(sockfd,IPPROTO_TCP,TCP_NODELAY, (char *) &val, sizeof(val)) == 0)
-            {
-              ipAddress.sin_family = entity->h_addrtype;
-              memcpy(&(ipAddress.sin_addr), entity->h_addr, entity->h_length);
-              ipAddress.sin_port = htons(80);
-
-              if(connect(sockfd,(struct sockaddr *) &ipAddress,sizeof(ipAddress)) == 0)
-                {
-                  if(write(sockfd,"GET /index.html HTTP/1.1\r\n\r\n", 29) >= 28)
-                    {
-                      shutdown(sockfd, SHUT_WR);
-                      if(read(sockfd, buffer, MAX_LINE) != -1) // all right!
-                        {
-                          //printf ("\nAll OKAY !!");
-                          close(sockfd);
-                          return true;
-                        }
-                      else
-                        report_error("read()");
-                    }
-                  else
-                    report_error("write()");
-                }
-              else
-                report_error("connect()");
-            }
-          else
-            report_error("setsockopt()");
+          sleep(1);
+          retry++;
         }
       else
-        report_error("socket()");
+        return;
     }
-  else
-    report_error("cannot resolve IP for Google."); // this is is the most common error.
-
-  if(sockfd!=-1)
-    close(sockfd); // no internet
-  return false;
 }
-
 
 bool tunnelExists()
 {
-  if (dummy_fail_count < 2)
-    return false;
-  else
+  if( access( tun0_device_file, F_OK ) != -1 )
     return true;
+  else
+    return false;
 }
 
 //Return value 1 means created successfully
-//0 means no success
+//0 means failure
 int createTunnel()
 {
-  sleep(1);//no op
-  if (dummy_fail_count < 2)
+  int retry = 0;
+  pid_of_openVPN_child_process = fork();
+  if (pid_of_openVPN_child_process == -1)
+    return 0;
+
+  if (pid_of_openVPN_child_process == 0)
     {
-      dummy_fail_count++;
-      return 0;
+      execlp (openVPN_binary,openVPN_params,ovpn_file_path);
     }
   else
-    {  return 1; //
+    {
+      while (retry < 500)
+        {
+          if (tunnelExists())
+            return 1;
+          else
+            sleep(1);
+          retry++;
+        }
+      return 0;
     }
-}
-
-void resetLeds ()
-{
-  red_led(off);
-  yellow_led(off);
-  blue_led(off);
 }
 
 
 int main ()
 {
-  red = fopen(red_led_file,"w");
-  blue = fopen(blue_led_file,"w");
-  yellow = fopen(yellow_led_file,"w");
+
   void *end;
-
-
 
   pthread_t vayvedThread_t,bThread_t;
   int vayvedThread,bThread;
 
   current_state = OFFLINE;
   currentColor = BLACK;
+  openLedDeviceFiles();
   resetLeds();
 
   bThread = pthread_create(&bThread_t, NULL,ledBlinkerThread, NULL);
